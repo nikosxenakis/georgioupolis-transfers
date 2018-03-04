492a25ab6c3e04c2fae3e81a418d62e36eade98e
Nikos Xenakis <nikosxenakis94@gmail.com>
2018-03-04 19:17:04 +0100

    dependencies added

--- src/app/providers/translate/translation.ts
+++ src/app/providers/translate/translation.ts
@@ -1,11 +1,11 @@
-import { OpaqueToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 
 // import translations
 import { LANG_EN_NAME, LANG_EN_TRANS } from './lang-en';
 import { LANG_GR_NAME, LANG_GR_TRANS } from './lang-gr';
 
 // translation token
-export const TRANSLATIONS = new OpaqueToken('translations');
+export const TRANSLATIONS = new InjectionToken('translations');
 
 // all translations
 export const dictionary = {
