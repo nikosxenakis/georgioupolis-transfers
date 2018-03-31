c28ea0b7ffac0a5dfafc218f163ff3db8f733a6a
Nikos Xenakis <nikosxenakis94@gmail.com>
2018-03-31 21:23:59 +0300

    Gramin effect removed

--- src/assets/scripts/init.js
+++ src/assets/scripts/init.js
@@ -21,21 +21,21 @@ window.onload = function() {
 		});
 	});
 
-	if(document.getElementById("canvasIndex"))
-		var granimInstance = new Granim({
-			element: '#canvasIndex',
-			direction: 'top-bottom',
-			opacity: [0.7, 0.6 , 0.9],
-			isPausedWhenNotInView: true,
-			states : {
-				"default-state": {
-					gradients: [
-						['#66582c', '#300d00'],	//dark
-						['#ad9e6d', '#300d00']	//light
-					],
-					transitionSpeed: 2000
-				}
-			}
-		});
+	// if(document.getElementById("canvasIndex"))
+	// 	var granimInstance = new Granim({
+	// 		element: '#canvasIndex',
+	// 		direction: 'top-bottom',
+	// 		opacity: [0.7, 0.6 , 0.9],
+	// 		isPausedWhenNotInView: true,
+	// 		states : {
+	// 			"default-state": {
+	// 				gradients: [
+	// 					['#66582c', '#300d00'],	//dark
+	// 					['#ad9e6d', '#300d00']	//light
+	// 				],
+	// 				transitionSpeed: 2000
+	// 			}
+	// 		}
+	// 	});
 
 }
